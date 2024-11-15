a7af5a23fe1460443fc3a23483a6c8e308847ede
Mahlst <peter.rothlaender.jr@gmail.com>
2024-11-15 16:25:54 +0100

    Add Section ProjectDetail

--- src/components/sections/ProjectDetail/ProjectDetail.jsx
+++ src/components/sections/ProjectDetail/ProjectDetail.jsx
@@ -2,6 +2,10 @@ import { useParams } from 'react-router-dom';
 import { projectsList } from '../../../data/projects';
 import './ProjectDetail.scss';
 import Header from '../../layout/Header/Header';
+import Footer from '../../layout/Footer/Footer';
+import SocialButton from '../../ui/Button/SocialButton';
+import { FaGithub } from "react-icons/fa";
+import { RiHexagonLine } from "react-icons/ri";
 
 export default function ProjectDetail() {
     const { id } = useParams();
@@ -18,10 +22,9 @@ export default function ProjectDetail() {
     return (
         <>
         <Header />
-        <section className="project-detail">
-            <h1>{project.title}</h1>
-            <img src={project.imageUrl} alt={project.title} />
-            <p>{project.description}</p>
+        <section className="project__detail">
+            <h1 className='project__detail-title'>Projet : {project.title}</h1>
+            <img className='project__detail-image' src={project.imageUrl} alt={project.title} />
             <div className="project__tags">
                 {project.tags.map((tag, index) => (
                     <span
@@ -34,7 +37,30 @@ export default function ProjectDetail() {
                     </span>
                 ))}
             </div>
+
+            <h2 className='project__detail-titleh2'>Contexte</h2>
+            <p className='project__detail-text'>{project.context}</p>
+
+
+            <h2 className='project__detail-titleh2'>Réalisation</h2>
+            <p className='project__detail-text'>{project.realization}</p>
+
+            <div className='project__detail-button'>
+                <SocialButton 
+                    href={project.github}
+                    icon={FaGithub}
+                    label="GitHub"
+                    variant="default"
+                />
+                <SocialButton 
+                    href={project.demo}
+                    icon={RiHexagonLine}
+                    label="Demo"
+                    variant="default"
+                />
+            </div>
         </section>
+        <Footer />
         </>
 
     );
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-import portrait from '../assets/portrait.jpg'
+import nina from '../assets/nina_carducci.jpg'
 import booki from '../assets/booki.jpg'
 import { FaHtml5 } from "react-icons/fa";
 import { IoLogoCss3, IoLogoJavascript } from "react-icons/io5";
@@ -8,30 +8,42 @@ export const projectsList = [
         id: "1",
         title: "Booki",
         description: "Créez la page d'accueil d'une agance de voyage avec HTML & CSS",
+        context: "La mission consiste à développer un site de recherche d'hébergements et d'activités touristiques pour la start-up Booki. Le développement se fait en collaboration avec Sarah (CTO) et Loïc (UI Designer). L'objectif est d'intégrer l'interface du site en HTML et CSS selon la maquette Figma fournie, en assurant une compatibilité responsive.",
+        realization: "Les ressources en ligne et les cours OpenClassrooms m'ont permis d'acquérir de nombreuses compétences pour réaliser ce projet. J'ai appris à comprendre une maquette Figma, structurer un site en HTML, utiliser Flexbox pour la mise en page CSS (notamment avec Flexbox Froggy), intégrer des media queries pour un design responsive, et versionner mon code avec Git et GitHub.",
         imageUrl: booki,
+        github: 'https://github.com/Mahlst/OC_P2',
+        demo: 'https://oc-p2-1cco0qsla-mahlsts-projects.vercel.app/',
         tags: [
             { 
                 label: "HTML", 
                 icon: <FaHtml5 />,
-                color: "#e65100"
+                color: "#e6975f"
             },
             { 
                 label: "CSS", 
                 icon: <IoLogoCss3 />,
-                color: "#1471b6"
+                color: "#00ffff"
             }
         ]
     },
     {
         id: "2",
-        title: "Autre Projet",
-        description: "Description de mon autre projet tout aussi intéressant.",
-        imageUrl: portrait,
+        title: "Nina Carducci",
+        description: "Débuggez et optimisez un site de photographe",
+        context: "Le projet consiste à optimiser globalement les performances et le référencement du site, mettre en place le référencement local avec Schema.org, ajouter les balises meta pour les réseaux sociaux, améliorer l'accessibilité et corriger des bugs de navigation dans la modale de la galerie et de sélection des filtres d'images. Le livrable sera un rapport détaillé présentant les actions réalisées, leur impact, les performances et l'accessibilité avant/après optimisation sur la base d'audits Lighthouse et Wave, ainsi que l'ensemble des modifications apportées au site.",
+        realization: "Des audits Lighthouse et Wave ont permis d'identifier les points d'amélioration du site. Les optimisations réalisées couvrent l'accessibilité (attributs alt, structuration hn et sémantique, footer, labels, contraste), les performances et le SEO (meta description, minification CSS/JS, optimisation des images, titre, données structurées Schema.org, metatags Open Graph et Twitter Cards). Le code a été débuggé pour la navigation modale et les filtres. De nouveaux audits ont été effectués après optimisation. Le rapport d'intervention détaille les actions et leur impact, incluant les résultats des audits avant/après.",
+        imageUrl: nina,
+        github: 'https://github.com/Mahlst/OC_P4',
+        demo: 'https://oc-p4-1mpznw4xy-mahlsts-projects.vercel.app/',
         tags: [
             { 
                 label: "JavaScript", 
                 icon: <IoLogoJavascript />,
                 color: "#F7DF1E"
+            },
+            { 
+                label: "SEO", 
+                color: "#42B983"
             }
         ]
     }

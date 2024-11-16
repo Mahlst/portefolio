1384f3517b58e91f3b379d33875667e72331009c
Mahlst <peter.rothlaender.jr@gmail.com>
2024-11-16 20:41:21 +0100

    Add more Skills

--- src/components/sections/Skill/Skill.jsx
+++ src/components/sections/Skill/Skill.jsx
@@ -3,6 +3,13 @@ import { FaReact, FaNodeJs } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { BsFiletypeScss } from "react-icons/bs";
 import { SiExpress, SiMongodb } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { TbSeo } from "react-icons/tb";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { VscVscode } from "react-icons/vsc";
+import { FaBrain } from "react-icons/fa";
 import './Skill.scss';
 
 export default function Skill() {
@@ -11,10 +18,16 @@ export default function Skill() {
             title: "Frontend",
             icons: [
                 {
-                    Icon: FaReact,
-                    color: '#61DAFB',
-                    name: 'React',
-                    desc: 'Librairie JavaScript pour construire des interfaces utilisateur'
+                    Icon: FaHtml5,
+                    color: '#E34F26',
+                    name: 'HTML',
+                    desc: 'Langage de balisage pour structurer et présenter le contenu web'
+                },
+                {
+                    Icon: FaCss3Alt,
+                    color: '#1572B6',
+                    name: 'CSS',
+                    desc: 'Langage de style pour mettre en forme et designer les pages web'
                 },
                 {
                     Icon: IoLogoJavascript,
@@ -23,11 +36,23 @@ export default function Skill() {
                     desc: 'Language de programmation polyvalent et dynamique'
                 },
                 {
+                    Icon: TbSeo,
+                    color: '#00A550',
+                    name: 'SEO',
+                    desc: 'Ensemble de techniques pour optimiser la visibilité d\'un site web dans les moteurs de recherche'
+                },
+                {
+                    Icon: FaReact,
+                    color: '#61DAFB',
+                    name: 'React',
+                    desc: 'Librairie JavaScript pour construire des interfaces utilisateur'
+                },
+                {
                     Icon: BsFiletypeScss,
                     color: '#CC6699',
                     name: 'SCSS',
-                    desc: 'Préprocesseur CSS avec variables et fonctions avancées'
-                }
+                    desc: 'Préprocesseur CSS qui ajoute des fonctionnalités avancées au CSS standard'
+                },
             ]
         },
         {
@@ -50,6 +75,35 @@ export default function Skill() {
                     color: '#4DB33D',
                     name: 'MongoDB',
                     desc: 'Base de données NoSQL orientée document'
+                }
+            ]
+        },
+        {
+            title: "Autres",
+            icons: [
+                {
+                    Icon: FaGitAlt,
+                    color: '#F05032',
+                    name: 'Git',
+                    desc: 'Système de contrôle de version pour le suivi des modifications du code'
+                },
+                {
+                    Icon: FaGithub,
+                    color: '#ffffff',
+                    name: 'Github',
+                    desc: 'Plateforme collaborative de gestion de code source et de projets'
+                },
+                {
+                    Icon: VscVscode,
+                    color: '#007ACC',
+                    name: 'VS Code',
+                    desc: 'Éditeur de code source puissant et personnalisable'
+                },
+                {
+                    Icon: FaBrain,
+                    color: '#9B59B6',
+                    name: 'Veille technologique',
+                    desc: 'Surveillance active des innovations et tendances dans le développement web'
                 }
             ]
         }
